fafc96b4658202c9d23fc1d98b5a14ba61585639
Rob Blackwell <rob.blackwell@cranfield.ac.uk>
2016-04-06 09:36:00 +0000

    Azure link working

--- src/do.py
+++ src/do.py
@@ -3,22 +3,26 @@ import serial
 
 usbport = '/dev/ttyAMA0'
 
-ser=""
+SER=""
 
 def connect ():
-    global ser
-    ser = serial.Serial(usbport, 9600, serial.EIGHTBITS, serial.PARITY_NONE)
+    global SER
+    SER = serial.Serial(usbport, 9600, serial.EIGHTBITS, serial.PARITY_NONE)
     
-def receive(ser=ser):
-  line = ""
-  while True:
-    data = ser.read().decode('utf-8')
-    if(data == "\r"):
-      return(line)
-    else:
-      line = line + data
+def receive(ser=None):
+    if ser is None:
+        ser = SER
+    line = ""
+    while True:
+        data = ser.read().decode('utf-8')
+        if(data == "\r"):
+            return(line)
+        else:
+            line = line + data
       
-def send(s, ser=ser):
-  ser.write(bytes(s,'utf-8'))
+def send(s, ser=None):
+    if ser is None:
+        ser = SER
+    ser.write(bytes(s,'utf-8'))
 
 

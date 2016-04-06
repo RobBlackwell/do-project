fada438e02806bf588b6252ddbc66439a59cd64c
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2016-04-06 11:14:09 +0100

    More commands

---
+++ src/do-info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import do
+
+do.VERBOSE = True
+
+# Connect to the Atlas Scientific DO circuit board
+do.connect()
+  
+do.send("I\r")
+do.receive()
+ok = do.receive()
+assert ok == '*OK'
+
---
+++ src/do-name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import do
+
+do.VERBOSE = True
+
+# Connect to the Atlas Scientific DO circuit board
+do.connect()
+  
+do.send("NAME,?\r")
+do.receive()
+ok = do.receive()
+assert ok == '*OK'
+
